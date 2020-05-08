36b1d3e2ac4fe3edd6b6393b90608d5c05cbe6ba
Sviatoslav Sentyurov <khirigiri@gmail.com>
2020-05-08 10:31:52 +0700

    Redact file main.c

--- main.c
+++ main.c
@@ -1,123 +1,51 @@
+#include <clocale>
+#include <cmath>
 #include <cstdlib>
 #include <iostream>
-#include <math.h>
-#include <string>
-
 using namespace std;
-
-typedef int c_characters[3];
-
-int Count(string enter)
-{
-    int counter = 0;
-    int temp = enter.find("circle(");
-    while (temp >= 0) {
-        counter++;
-        int p = enter.find_first_of("(");
-        enter.erase(0, p + 1);
-        string c = "";
-        c.append(enter.substr(0, 1));
-        enter.erase(0, 1);
-
-        int f = c.find_first_of(")");
-
-        while (f < 0) {
-            c.append(enter.substr(0, 1));
-            enter.erase(0, 1);
-            f = c.find_first_of(")");
-        }
-        int comma = c.find_first_of(",");
-
-        c.erase(f);
-
-        f = c.find_first_of(" ");
-        temp = enter.find("circle(");
-    }
-    return counter;
-}
-
+const float PI = 3.14;
 int main()
 {
-    string enter;
-    getline(cin, enter);
-    int x, y, r, i = 0;
-    c_characters* mas;
-    int razmer = Count(enter);
-    int temp = enter.find("circle(");
-    mas = new c_characters[razmer];
-
-    while (razmer - i > 0) {
-        int p = enter.find_first_of("(");
-        enter.erase(0, p + 1);
-        string c = "";
-        c.append(enter.substr(0, 1));
-        enter.erase(0, 1);
-
-        int f = c.find_first_of(")");
-
-        while (f < 0) {
-            c.append(enter.substr(0, 1));
-            enter.erase(0, 1);
-            f = c.find_first_of(")");
-        }
-        int comma = c.find_first_of(",");
-
-        c.erase(f);
-
-        f = c.find_first_of(" ");
-
-        string xstr = "";
-        string ystr = "";
-        string rstr = "";
-        rstr.append(c.substr(comma + 1));
-        xstr.append(c.substr(0, f));
-        ystr.append(c.substr(f + 1, comma));
-
-        r = atoi(rstr.c_str());
-        x = atoi(xstr.c_str());
-        y = atoi(ystr.c_str());
-
-        mas[i][0] = x;
-        mas[i][1] = y;
-        mas[i][2] = r;
-        i++;
-
-        temp = enter.find("circle(");
+    setlocale(LC_ALL, "RUS");
+    int n, i, j;
+    float **a, x, y, R, c = 0;
+    cout << "Введите количество окружностей: " << endl;
+    cin >> n;
+    a = new float*[n];
+    for (i = 0; i < n; i++) {
+        a[i] = new float[5];
+        cout << endl << "Введите характеристики окружности №" << i + 1 << endl;
+        cout << "Координата (x) окружности" << endl;
+        cin >> a[i][1];
+        cout << "Координата (y) окружности" << endl;
+        cin >> a[i][2];
+        cout << "Радиус окружности" << endl;
+        cin >> a[i][3];
+        a[i][4] = PI * (pow(a[i][3], 2));
+        a[i][5] = 2 * PI * a[i][3];
     }
     cout << endl;
-    for (int u = 0; u < i; u++) {
-        for (int h = 0; h < 3; h++) {
-            cout << mas[u][h] << " ";
-        }
-        cout << endl;
-        cout << endl;
-    }
-
-    for (int u = 0; u < i; u++) {
-        double per, sq;
-        per = 2 * mas[u][2] * 3.1415;
-        sq = 3.1415 * mas[u][2] * mas[u][2];
-        cout << u + 1 << " Krug " << mas[u][0] << " " << mas[u][1] << endl;
-        cout << "Perimetr = " << per << endl;
-        cout << "Ploshad' = " << sq << endl;
-        for (int g = 0; g < i; g++) {
-            int rast, sumR;
-            if (g == u)
-                g++;
-            if (g == i)
-                break;
-            rast
-                    = sqrt(pow(mas[u][0] - mas[g][0], 2)
-                           + pow(mas[u][1] - mas[g][1], 2));
-            sumR = mas[u][2] + mas[g][2];
-            if (rast <= sumR) {
-                cout << "Peresekaetsya s " << g + 1 << " krugom" << endl;
-            } else {
-                cout << "Ne peresekaetsya s " << g + 1 << " krugom" << endl;
+    for (i = 0; i < n - 1; i++)
+        for (j = i + 1; j < n; j++) {
+            x = abs(a[i][1] - a[j][1]);
+            y = abs(a[i][2] - a[j][2]);
+            R = a[i][3] + a[j][3];
+            if ((x < R) && (y < R)) {
+                c++;
+                cout << "Окружности №" << i + 1 << " и №" << j + 1
+                     << " пересекаются" << endl;
             }
         }
-        cout << endl << endl;
+    cout << endl;
+    for (i = 0; i < n; i++) {
+        cout << "Характерисики окружности №" << i + 1 << ":" << endl;
+        cout << "Координаты центра: (" << a[i][1] << "," << a[i][2] << ")"
+             << endl;
+        cout << "Радиус: " << a[i][3] << endl;
+        cout << "Площадь: " << a[i][4] << endl;
+        cout << "Периметр: " << a[i][5] << endl;
+        cout << endl;
     }
-    system("PAUSE");
-    return 0;
+    if (c == 0)
+        cout << "Пересекающихся окружностей не найдено" << endl;
 }
