a2094306751114c87ece5ae0985889a68f062a1a
Sviatoslav Sentyurov <khirigiri@gmail.com>
2020-05-10 15:54:57 +0700

    Redact calc.cpp

--- src/calc.cpp
+++ src/calc.cpp
@@ -3,8 +3,8 @@ int calc(int i, int j, float** a, int c)
 {
     float x, y, R;
     //исследование на пересечение окружностей i и j
-    x = abs(a[i][0] - a[j][0]);
-    y = abs(a[i][1] - a[j][1]);
+    x = std::abs(a[i][0] - a[j][0]);
+    y = std::abs(a[i][1] - a[j][1]);
     R = a[i][2] + a[j][2];
     if ((x < R) && (y < R))
         c++;
